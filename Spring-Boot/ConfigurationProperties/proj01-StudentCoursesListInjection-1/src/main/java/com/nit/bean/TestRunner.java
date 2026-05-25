992f20a20932d8e6d4ee0490e6da6e429fbda7c7
Taurej Alam <tabrezalam8133@gmail.com>
2026-05-26 00:57:50 +0530

    Add files via upload

---
+++ Spring-Boot/ConfigurationProperties/proj01-StudentCoursesListInjection-1/src/main/java/com/nit/bean/TestRunner.java
@@ -0,0 +1,21 @@
+package com.nit.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.nit.bean.Student;
+
+@Component
+public class TestRunner implements CommandLineRunner {
+
+    @Autowired
+    private Student info;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println(info);
+
+    }
+}
