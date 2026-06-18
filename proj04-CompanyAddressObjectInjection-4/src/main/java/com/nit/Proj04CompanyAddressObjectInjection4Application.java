d05bbc19519e88a204eb3466d827cd3e04dec994
Taurej Alam <tabrezalam8133@gmail.com>
2026-06-18 23:56:23 +0530

    Add files via upload

---
+++ proj04-CompanyAddressObjectInjection-4/src/main/java/com/nit/Proj04CompanyAddressObjectInjection4Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj04CompanyAddressObjectInjection4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj04CompanyAddressObjectInjection4Application.class, args);
+	}
+
+}
---
+++ proj04-CompanyAddressObjectInjection-4/src/main/java/com/nit/bean/Address.java
@@ -0,0 +1,40 @@
+package com.nit.bean;
+
+
+public class Address {
+
+    private String city;
+    private String state;
+    private Integer pincode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(Integer pincode) {
+        this.pincode = pincode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address [city=" + city +
+                ", state=" + state +
+                ", pincode=" + pincode + "]";
+    }
+}
