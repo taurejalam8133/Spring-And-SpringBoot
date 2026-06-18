35fc7003ef2daf78e1eec83548febac123696653
Taurej Alam <tabrezalam8133@gmail.com>
2026-06-18 23:55:34 +0530

    Add files via upload

---
+++ proj03-ProductFeaturesMapInjection-3/src/main/java/com/nit/Proj03ProductFeaturesMapInjection3Application.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj03ProductFeaturesMapInjection3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj03ProductFeaturesMapInjection3Application.class, args);
+	}
+
+}
---
+++ proj03-ProductFeaturesMapInjection-3/src/test/java/com/nit/Proj03ProductFeaturesMapInjection3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proj03ProductFeaturesMapInjection3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
